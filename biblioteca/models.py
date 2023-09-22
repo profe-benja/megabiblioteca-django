7b968f4bd791859ef76b8985b37cb2a999ac0c3a
profe benja <bej.mora@profesor.duoc.cl>
2023-09-22 17:30:08 -0300

    rutas y modelos

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100)
+
+class Libro(models.Model):
+    nombre = models.CharField(max_length=200)
+    codigo = models.CharField(max_length=20, unique=True)
+    descripcion = models.TextField()
+    imagen = models.ImageField(upload_to='libros/')
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+class EstadoPedido(models.Model):
+    nombre = models.CharField(max_length=20)
+
+class Pedido(models.Model):
+    libro = models.ForeignKey(Libro, on_delete=models.CASCADE)
+    cliente = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    estado = models.ForeignKey(EstadoPedido, on_delete=models.CASCADE)
--- biblioteca/views.py
+++ biblioteca/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Libro, Pedido, EstadoPedido
 
-# Create your views here.
+# inicio de todo
+def index(request):
+    return render(request, 'index.html')
+
+# AUTH
+def inicio_sesion(request):
+    return render(request, 'biblioteca/index.html')
+
+def cerrar_sesion(request):
+    return render(request, 'biblioteca/index.html')
+
+# USUARIO
+
+# LIBRO
+def lista_libros(request):
+    libros = Libro.objects.all()
+
+    context = {
+        'libros' : libros
+    }
+    
+    return render(request, 'biblioteca/lista_libros.html', context)
+
+def detalle_libro(request, libro_id):
+    libro = get_object_or_404(Libro, pk=libro_id)
+    
+    context = {
+        'libro' : libro
+    }
+    
+    return render(request, 'biblioteca/detalle_libro.html', context)
+
+def solicitar_libro(request, libro_id):
+    libro = get_object_or_404(Libro, pk=libro_id)
+    # Aquí puedes manejar la lógica de solicitud de libros
+    return redirect('lista_libros')
