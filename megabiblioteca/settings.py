143dfbb462cb04b0f32724a9ba0a392910e39e09
profe benja <bej.mora@profesor.duoc.cl>
2023-09-29 22:56:56 -0300

    Se añadio token

--- megabiblioteca/settings.py
+++ megabiblioteca/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'biblioteca',
     'rest_framework',
-    'rest_api'
+    'rest_api',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
@@ -103,6 +104,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',  # Agrega esta línea para la autenticación de tokens
+    ],
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
